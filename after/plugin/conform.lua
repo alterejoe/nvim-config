58254011ae76f5e44bd387e3914c8b241be8d4d0
altjoe <meyer.joer5@gmail.com>
2024-07-24 14:36:41 -0400

    prettier for wiki-md

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -6,10 +6,22 @@ conform.setup({
 		lua = { "stylua" },
 		python = { "autopep8" },
 		go = { "goimports" },
-		vimwiki = { "mdformat" },
+		vimwiki = { "prettier" },
+		-- ["*"] = { "codespell" },
+		-- -- Use the "_" filetype to run formatters on filetypes that don't
+		-- -- have other formatters configured.
+		-- ["_"] = { "trim_whitespace" },
 	},
 	default_formatters = { "efm" },
 })
+
+require("conform").formatters.prettier = {
+	options = {
+		ft_parsers = {
+			vimwiki = "markdown",
+		},
+	},
+}
 
 vim.api.nvim_create_augroup("ConformAutogroup", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
