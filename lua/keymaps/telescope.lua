46333d84e6b43e2ea075db6931b0c2dab904097b
hpc <joe@alterjoe.com>
2025-07-08 21:08:00 -0400

    fd corrections for hidden files and max depth

--- lua/keymaps/telescope.lua
+++ lua/keymaps/telescope.lua
@@ -1,15 +1,34 @@
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>F", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>rf", telescope.extensions.recent_files.recent_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>ff", telescope.extensions.recent_files.pick, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", telescope.extensions.egrepify.egrepify, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>mm", telescope.extensions.messages.messages, { desc = "Telescope messages" })
+-- vim.keymap.set("n", "<leader>F", builtin.find_files, { desc = "Telescope find files" })
+-- vim.keymap.set("n", "<leader>rf", telescope.extensions.recent_files.recent_files, { desc = "Telescope find files" })
+-- vim.keymap.set("n", "<leader>ff", telescope.extensions.recent_files.pick, { desc = "Telescope find files" })
+-- vim.keymap.set("n", "<leader>fg", telescope.extensions.egrepify.egrepify, { desc = "Telescope live grep" })
+-- vim.keymap.set("n", "<leader>mm", telescope.extensions.messages.messages, { desc = "Telescope messages" })
+
+vim.keymap.set("n", "<leader>rf", function()
+	require("telescope").extensions.recent_files.recent_files({ cwd = vim.fn.getcwd() })
+end, { desc = "Telescope find recent files" })
+
+vim.keymap.set("n", "<leader>ff", function()
+	require("telescope.builtin").find_files({
+		cwd = vim.fn.getcwd(),
+		hidden = true,
+		max_depth = 3,
+	})
+end, { desc = "Telescope pick a file" })
+
+vim.keymap.set("n", "<leader>fg", function()
+	require("telescope").extensions.egrepify.egrepify({ cwd = vim.fn.getcwd() })
+end, { desc = "Telescope live grep" })
+
+vim.keymap.set("n", "<leader>mm", function()
+	require("telescope").extensions.messages.messages({ cwd = vim.fn.getcwd() })
+end, { desc = "Telescope messages" })
 
 vim.keymap.set("n", "<leader>fw", function()
 	local word = vim.fn.expand("<cword>")
 	require("telescope.builtin").live_grep({ default_text = word })
 end)
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-print(vim.inspect(require("telescope").extensions))
