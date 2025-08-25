80db271c08d27a67fcfa8b2e642b81aa02df2997
hpc <joe@alterjoe.com>
2025-08-25 02:28:25 -0400

    piece of shit

--- lua/keymaps/jump.lua
+++
@@ -1,5 +0,0 @@
--- Go to previous buffer
-vim.keymap.set("n", "<C-o>", "<cmd>bprevious<CR>", { noremap = true, silent = true })
-
--- Go to next buffer
-vim.keymap.set("n", "<C-i>", "<cmd>bnext<CR>", { noremap = true, silent = true })
