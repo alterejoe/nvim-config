42adda523766e5539c3ea49a372b1b02c1620387
j.meyer <meyer.joer5@gmail.com>
2025-11-19 06:36:02 +0000

    revert to previous air and new go dap configs for flexible main.go dir

--- lua/plugins/configs/nvim-dap-configs.lua
+++ lua/plugins/configs/nvim-dap-configs.lua
@@ -92,28 +92,70 @@ local function GetTestFunctionNames(filepath)
 		return { "-test.v" }
 	end
 end
-
 dap.configurations.go = {
+	-- Dynamic: Debug the cmd/* directory containing current file
 	{
 		type = "dlv_spawn",
-		name = "cmd/main.go w/ deps",
+		name = "Debug current cmd package",
 		request = "launch",
 		mode = "debug",
 		program = function()
-			return vim.g.dap_path or "./cmd/"
+			local filepath = vim.fn.expand("%:p:h") -- :h gets directory, not file
+			local cwd = vim.fn.getcwd()
+			-- Find the cmd/runner* directory containing this file
+			local cmd_dir = filepath:match("(.*cmd/[^/]+)")
+			if cmd_dir then
+				print("Debugging: ", cmd_dir)
+				return cmd_dir
+			end
+			-- Fallback to ./cmd/ if not in a cmd subdirectory
+			return cwd .. "/cmd/"
+		end,
+		cwd = function()
+			return vim.fn.getcwd()
 		end,
 		args = { "-v" },
 	},
+	-- Interactive: Choose which runner to debug
 	{
 		type = "dlv_spawn",
-		name = "cmd/main.go w/o tailwind and templ",
+		name = "Select cmd runner to debug",
 		request = "launch",
 		mode = "debug",
 		program = function()
-			return "./cmd/"
+			local cwd = vim.fn.getcwd()
+			local cmd_dirs = vim.fn.glob(cwd .. "/cmd/*/", false, true)
+
+			if #cmd_dirs == 0 then
+				return cwd .. "/cmd/"
+			end
+
+			-- Strip trailing slashes and make relative for display
+			local choices = vim.tbl_map(function(dir)
+				return dir:gsub("/$", ""):gsub(cwd .. "/", "")
+			end, cmd_dirs)
+
+			local choice = vim.fn.inputlist(vim.list_extend(
+				{ "Select runner to debug:" },
+				vim.tbl_map(function(i)
+					return i .. ". " .. choices[i]
+				end, vim.fn.range(1, #choices))
+			))
+
+			if choice > 0 and choice <= #choices then
+				local selected = cmd_dirs[choice]:gsub("/$", "")
+				print("Debugging: ", selected)
+				return selected
+			end
+			return cwd .. "/cmd/"
+		end,
+		cwd = function()
+			return vim.fn.getcwd()
 		end,
 		args = { "-v" },
 	},
+
+	-- Test current file
 	{
 		type = "dlv_spawn",
 		name = "Test current file",
@@ -130,18 +172,8 @@ dap.configurations.go = {
 			return args
 		end,
 	},
-	{
-		type = "dlv_spawn",
-		name = "Run single file",
-		request = "launch",
-		mode = "debug",
-		program = function()
-			print(vim.fn.expand("%:p:h"))
-			local cwd = vim.fn.getcwd()
-			print("cwd: ", cwd)
-			return vim.fn.expand("%:p:h")
-		end,
-	},
+
+	-- Debug from current working directory
 	{
 		type = "dlv_spawn",
 		name = "Run main.go within current directory",
@@ -158,6 +190,72 @@ dap.configurations.go = {
 		end,
 	},
 }
+--
+-- dap.configurations.go = {
+-- 	{
+-- 		type = "dlv_spawn",
+-- 		name = "cmd/main.go w/ deps",
+-- 		request = "launch",
+-- 		mode = "debug",
+-- 		program = function()
+-- 			return vim.g.dap_path or "./cmd/"
+-- 		end,
+-- 		args = { "-v" },
+-- 	},
+-- 	{
+-- 		type = "dlv_spawn",
+-- 		name = "cmd/main.go w/o tailwind and templ",
+-- 		request = "launch",
+-- 		mode = "debug",
+-- 		program = function()
+-- 			return "./cmd/"
+-- 		end,
+-- 		args = { "-v" },
+-- 	},
+-- 	{
+-- 		type = "dlv_spawn",
+-- 		name = "Test current file",
+-- 		request = "launch",
+-- 		mode = "test",
+-- 		program = function()
+-- 			return vim.fn.expand("%:p:h")
+-- 		end,
+-- 		args = function()
+-- 			local filepath = vim.fn.expand("%:p")
+-- 			print("filepath: ", filepath)
+-- 			local args = GetTestFunctionNames(filepath)
+-- 			print("args: ", args)
+-- 			return args
+-- 		end,
+-- 	},
+-- 	{
+-- 		type = "dlv_spawn",
+-- 		name = "Run single file",
+-- 		request = "launch",
+-- 		mode = "debug",
+-- 		program = function()
+-- 			print(vim.fn.expand("%:p:h"))
+-- 			local cwd = vim.fn.getcwd()
+-- 			print("cwd: ", cwd)
+-- 			return vim.fn.expand("%:p:h")
+-- 		end,
+-- 	},
+-- 	{
+-- 		type = "dlv_spawn",
+-- 		name = "Run main.go within current directory",
+-- 		request = "launch",
+-- 		mode = "debug",
+-- 		program = function()
+-- 			local cwd = vim.fn.getcwd()
+-- 			print("Current working directory: ", cwd)
+-- 			return cwd .. "/"
+-- 		end,
+-- 		cwd = function()
+-- 			local filedir = vim.fn.expand("%:p:h")
+-- 			return filedir
+-- 		end,
+-- 	},
+-- }
 
 dap.configurations.templ = {
 	{
